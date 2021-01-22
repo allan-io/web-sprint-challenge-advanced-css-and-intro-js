1daeffe80a5aa8e914a62efb0611eaca8cefc179
Allan <allanoliveira.dev@gmail.com>
2021-01-22 13:56:31 -0500

    allan oliveira

--- index.js
+++ index.js
@@ -378,17 +378,30 @@ function getHTML(data){
     }
   }
 
-  getHTML(artists)
+  // getHTML(artists)
 
 
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
+function randomize(arr){
 
-    /* Code here */
+    let currentIndex = arr.length, tempVal, randomIndex
 
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+
+      tempVal = arr[currentIndex]
+      arr[currentIndex] = arr[randomIndex]
+      arr[randomIndex] = tempVal
+    }
+
+    return arr
   }
+
+ randomize(artists)
+ console.log(artists)
 
 
  /* 💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪
